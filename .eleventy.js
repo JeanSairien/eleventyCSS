2cff42b7f57d07fa0e1e8c67114fd8313535ac51
DrRedfish <lesonduko@gmail.com>
2020-12-12 18:00:05 +0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
-  return {
-    passthroughFileCopy: true
-  }
-}
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('js')
-  return {
-    passthroughFileCopy: true
-  }
-}
-
-module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('fonts')
+  eleventyConfig.addPassthroughCopy('js')
   return {
     passthroughFileCopy: true
   }
